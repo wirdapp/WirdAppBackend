a0dfb3bb640392659000c8ac951f25b2a881aee2
osama-abuhamdan <OHamdan@1997>
2022-02-12 23:21:40 -0800

    - Add change password view

--- core/views.py
+++ core/views.py
@@ -1,8 +1,22 @@
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 25
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+
+class ChangePasswordSerializer(viewsets.ModelViewSet):
+    @action(detail=True, methods=['put'], name='Change Password')
+    def change_password(self, request, *args, **kwargs):
+        user = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({**serializer.errors})
+        serializer.update(user, serializer.validated_data)
+        return Response({**serializer.data})
