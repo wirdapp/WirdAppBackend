5ed9358b44a1e7ea7713141acdf7335aa9298b93
osama-abuhamdan <OHamdan@1997>
2022-02-13 23:06:12 -0800

    Moved student control views to admin project

--- student/views.py
+++ student/views.py
@@ -1,11 +1,8 @@
-from django.contrib.auth.models import AnonymousUser
-from django.db.models import QuerySet
-from rest_condition import Or
 from rest_framework import permissions, viewsets
-from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from core.permissions import IsCompetitionAdmin
-from core.views import StandardResultsSetPagination, ChangePasswordSerializer
+from core.permissions import NoPermission
+from core.views import StandardResultsSetPagination
 from .serializers import *
 
 
@@ -19,61 +16,34 @@ class PointRecordsView(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        date = self.request.query_params.get('ramadan_record_date', 0)
-        student_username = self.request.query_params.get('student_username', '0')
+        date = self.request.query_params.get('ramadan_record_date', 1)
         if hasattr(user, 'competition_students'):
-            return user.competition_students.student_points.filter(ramadan_record_date=date)
-        elif hasattr(user, 'competition_admins'):
-            students = StudentUserView(context={'request': self.request}, request=self.request).get_queryset()
-            points = students.get(username=student_username).student_points().filter(ramadan_record_date=date)
-            return points
+            return StudentUser.objects.get(username=user.username).student_points.filter(ramadan_record_date=date)
+        else:
+            return PointRecord.objects.none()
 
 
-class StudentUserView(ChangePasswordSerializer):
+class StudentUserView(viewsets.ModelViewSet):
     pagination_class = StandardResultsSetPagination
     name = 'student-user-list'
-    lookup_field = 'username'
 
     def get_queryset(self):
         user = self.request.user
-        competition = user.competition
-        if isinstance(user, AnonymousUser):
-            return StudentUser.objects.none()
-        if user.is_staff:
-            return StudentUser.objects.all()
         if hasattr(user, 'competition_students'):
-            return QuerySet(user.competition_students).filter(username=user.username)
-        if hasattr(user, 'competition_admins'):
-            admin = user.competition_admins
-            if admin.is_super_admin:
-                return competition.competition_students.all()
-            else:
-                try:
-                    groups = competition.competition_groups.filter(admin=admin)
-                    res = StudentUser.objects.none()
-                    merge_sets = lambda x, y: x | y
-                    for g in groups:
-                        res = merge_sets(res, g.group_students.all())
-                    return res
-                except:
-                    return StudentUser.objects.none()
+            return StudentUser.objects.get(username=user.username)
+        else:
+            return StudentUser.objects.none()
 
     def get_serializer_class(self):
-        if self.action in ['create', 'list']:
+        if self.action in ['create']:
             return StudentUserSerializer
-        elif self.action == "change_password":
-            return StudentUserUpdatePasswordSerializer
         elif self.action in ['update', 'partial_update', 'retrieve']:
-            user = self.request.user
-            if hasattr(user, 'competition_students'):
-                return StudentUserStudentUpdateSerializer
-            else:
-                return StudentUserAdminUpdateSerializer
+            return StudentUpdateSerializer
 
     def get_permissions(self):
-        if self.action == 'create':
+        if self.action in ['create']:
             return AllowAny(),
         elif self.action in ['retrieve', 'update', 'partial_update']:
             return IsAuthenticated(),
         else:
-            return Or(IsAdminUser(), IsCompetitionAdmin()),
+            return NoPermission(),
