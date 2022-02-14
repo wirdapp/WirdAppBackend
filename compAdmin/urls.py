ccec319fdad2ea41348491f4fc730f19471eceec
osama-abuhamdan <OHamdan@1997>
2022-02-13 23:05:25 -0800

    Moved student control views to admin project

--- compAdmin/urls.py
+++ compAdmin/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from . import views
+from . import student_views
 
 router = routers.DefaultRouter()
 router.register(r'comp-admins', views.CompAdminView, basename='CompAdmin')
@@ -9,6 +10,7 @@ router.register(r'comp-group', views.CompGroupView, basename='CompGroup')
 router.register(r'point-template', views.PointTemplatesView, basename='PointTemplate')
 # router.register(r'point-format', views.PointFormatView, basename='PointFormat') #TODO: to admin view only
 router.register(r'section', views.SectionView, basename='Section')
+router.register(r'students', student_views.StudentView, basename='Students')
 
 urlpatterns = [
     path('', include(router.urls)),
