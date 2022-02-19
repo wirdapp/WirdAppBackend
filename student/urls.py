db25958408254ca4e0daa3576eb23428107efcb7
osama-abuhamdan <OHamdan@1997>
2022-02-19 05:00:27 -0800

    Add 'point-templates' and 'advertisements' urls

--- student/urls.py
+++ student/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'student-user', views.StudentUserView, basename='StudentUser')
-router.register(r'record-point', views.PointRecordsView, basename='PointRecord')
+router.register(r'point-records', views.PointRecordsView, basename='PointRecord')
+router.register(r'point-templates', views.PointTemplatesView, basename='StudentPointTemplate')
+router.register(r'advertisements', views.AdvertisementsView, basename='Advertisements')
 
 urlpatterns = [
     path('', include(router.urls)),
