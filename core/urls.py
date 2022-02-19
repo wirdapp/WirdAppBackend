f014890b5a8dca86a8567c8876c1abd333a1d138
osama-abuhamdan <OHamdan@1997>
2022-02-19 04:59:55 -0800

    Change competitions URL basename from CompAdmin to Competitions to avoid conflicts

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'competition', views.CompetitionView, basename='CompAdmin')
+router.register(r'competition', views.CompetitionView, basename='Competitions')
 
 urlpatterns = [
     path('', include(router.urls)),
