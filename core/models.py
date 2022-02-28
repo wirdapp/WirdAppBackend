190ac5c2efc0ad48c02e893bfe1f13903e0c308c
osama-abuhamdan <OHamdan@1997>
2022-02-27 22:26:20 -0800

    Create a view for competition

--- core/models.py
+++ core/models.py
@@ -4,8 +4,19 @@ from django.db import models
 
 # Create your models here.
 
+class Competition(models.Model):
+    id = models.CharField(max_length=30, primary_key=True, default='')
+    name = models.CharField(max_length=30, default='')
+    show_standings = models.BooleanField(default=True)
+    announcements = models.TextField(default="", blank=True)
+    readonly_mode = models.BooleanField(default=False, help_text='Stop scoring points and only show scored points')
+
+    def __str__(self):
+        return self.name
+
+
 class GeneralUser(AbstractUser):
-    competition = models.ForeignKey('compAdmin.Competition', on_delete=models.CASCADE, null=True)
+    competition = models.ForeignKey(Competition, on_delete=models.CASCADE, null=True)
 
     def set_competition(self, competition):
         self.competition = competition
