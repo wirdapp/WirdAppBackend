d991eaac8632effffdf0e817d38e3ef92e852b9b
osama-abuhamdan <OHamdan@1997>
2022-03-19 22:19:43 -0700

    Check if the user is updating or the student

--- student/serializers.py
+++ student/serializers.py
@@ -6,20 +6,26 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from compAdmin.models import Competition
-from compAdmin.student_serializers import ReadOnlyPointTemplateSerializer
 from core.serializers import CompetitionFilteredPrimaryKeyRelatedField
 from .models import *
 
 
+class ReadOnlyPointTemplateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PointTemplate
+        depth = 2
+        fields = ['id', 'label', "description", "form_type", "upper_units_bound", "lower_units_bound",
+                  "points_per_unit"]
+
+
 class PointRecordSerializer(serializers.ModelSerializer):
-    point_template = CompetitionFilteredPrimaryKeyRelatedField(clazz=PointTemplate,
-                                                               serializer=ReadOnlyPointTemplateSerializer)
+    point_template = CompetitionFilteredPrimaryKeyRelatedField(clazz=PointTemplate, serializer=ReadOnlyPointTemplateSerializer)
     student = serializers.CharField(read_only=True, source='student.username')
 
     class Meta:
         model = PointRecord
         depth = 0
-        fields = "__all__"
+        exclude = ('user_input',)
         extra_kwargs = {'point_total': {'read_only': True}}
 
     def validate(self, attrs):
@@ -27,15 +33,17 @@ class PointRecordSerializer(serializers.ModelSerializer):
         point_template = attrs['point_template']
         scored_units = attrs['point_scored_units']
         record_date = attrs['ramadan_record_date']
-        user = self.context['request'].user.competition_students
+        user = self.context['request'].user
         competition = user.competition
+        if hasattr(user, 'competition_students'):
+            student = user.competition_students
+            self.check(not (student.read_only or competition.readonly_mode), 'You can\'t score new points', errors, 'General')
         self.check(point_template.is_active and point_template.is_shown, 'Point is not active', errors,
                    'Point template')
         self.check(point_template.upper_units_bound >= scored_units >= point_template.lower_units_bound,
                    'Point is beyond limits', errors, 'Point scored units')
         self.check(30 >= record_date >= 1, 'Date is beyond limits', errors, 'Ramadan record date')
         self.check(30 >= record_date >= 1, 'Date is beyond limits', errors, 'Ramadan record date')
-        self.check(not (user.read_only or competition.readonly_mode), 'You can\'t score new points', errors, 'General')
         if point_template.custom_days:
             self.check(str(record_date) in point_template.custom_days.split(','), 'Point is not active on this day',
                        errors, 'Ramadan record date')
