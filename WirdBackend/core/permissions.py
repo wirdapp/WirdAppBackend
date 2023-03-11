2f729324b24c5a35924e8a332b451ab0a3e950c0
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:51 -0800

    Rename util to util_methods

--- WirdBackend/core/permissions.py
+++ WirdBackend/core/permissions.py
@@ -6,7 +6,7 @@ from collections.abc import Iterable
 
 from rest_framework.permissions import BasePermission
 
-from core import util, models_helper
+from core import util_methods, models_helper
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +15,7 @@ def person_role_in_contest(request, expected_roles):
     try:
         if not isinstance(expected_roles, Iterable):
             expected_roles = [expected_roles]
-        current_contest = util.get_current_contest_dict(request)
+        current_contest = util_methods.get_current_contest_dict(request)
         return bool(current_contest["role"] in expected_roles)
     except Exception as e:
         logger.error("Error while getting person role in contest", e)
@@ -35,8 +35,8 @@ class IsContestAdmin(BasePermission):
 class IsGroupAdmin(BasePermission):
     #TODO: Support more variations
     def has_permission(self, request, view):
-        contest_id = util.get_current_contest_dict(request)["id"]
-        username = util.get_username_from_session(request)
+        contest_id = util_methods.get_current_contest_dict(request)["id"]
+        username = util_methods.get_username_from_session(request)
         group_id = view.kwargs["group_id"]
         return models_helper.get_person_managed_groups(username, contest_id).filter(id=group_id).exists()
 
