2c0983ff9153b44b92c5e9d4e80be00db2ec2aa4
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:44 -0800

    Cleanup permissions

--- WirdBackend/core/permissions.py
+++ WirdBackend/core/permissions.py
@@ -1,43 +1,40 @@
 """
 Provides a set of pluggable permission policies.
 """
+import logging
 from collections.abc import Iterable
 
 from rest_framework.permissions import BasePermission
 
 from core import util
 
+logger = logging.getLogger(__name__)
+
 
 def person_role_in_contest(request, expected_roles):
-    if not isinstance(expected_roles, Iterable):
-        expected_roles = [expected_roles]
-    current_contest = util.get_current_contest_dict(request)
-    return bool(current_contest["role"] in expected_roles)
+    try:
+        if not isinstance(expected_roles, Iterable):
+            expected_roles = [expected_roles]
+        current_contest = util.get_current_contest_dict(request)
+        return bool(current_contest["role"] in expected_roles)
+    except Exception as e:
+        logger.error("Error while getting person role in contest", e)
+        return False
 
 
-# TODO: Remove is_staff part from the conditions
 class IsContestMember(BasePermission):
     def has_permission(self, request, view):
-        try:
-            return request.user.is_staff or person_role_in_contest(request, [3, 2, 1])
-        except:
-            return False
+        return person_role_in_contest(request, [3, 2, 1])
 
 
 class IsContestAdmin(BasePermission):
     def has_permission(self, request, view):
-        try:
-            return request.user.is_staff or person_role_in_contest(request, [3, 2])
-        except:
-            return False
+        return person_role_in_contest(request, [3, 2])
 
 
 class IsContestSuperAdmin(BasePermission):
     def has_permission(self, request, view):
-        try:
-            return request.user.is_staff or person_role_in_contest(request, [3])
-        except:
-            return False
+        return person_role_in_contest(request, [3])
 
 
 class NoPermission(BasePermission):
