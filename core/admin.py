acef4cc8082668234e08a808108cc73a86945e78
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:43 -0800

    Organize Files

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from core.models import GeneralUser, Competition
-
-admin.site.register(GeneralUser)
-admin.site.register(Competition)
--- start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-#python manage.py makemigrations
-#python manage.py migrate
-gunicorn Ramadan_Competition_Rest.wsgi -c ./gunicorn/prod.py -b 0.0.0.0:8000
