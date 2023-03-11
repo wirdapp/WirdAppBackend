a2d3eb9491234a3452e3968bf05d98a4113e3fa5
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:45 -0800

    Cleanup and Extension

--- WirdBackend/core/util.py
+++ WirdBackend/core/util.py
@@ -1,10 +1,10 @@
 import hashlib
 
-from django.core.cache import cache
 from hijri_converter import Hijri
 from rest_framework.response import Response
 
-from core.models import ContestPerson
+from core import models_helper
+from core.models import Contest
 
 
 def hash_multiple_values(*args):
@@ -21,37 +21,25 @@ def destroy(instance):
     return Response(status=204)
 
 
-def get_user_contest_person_objects(request):
-    username = request.user.username
-    key = hash_multiple_values(username)
-    objects = cache.get(key)
-    if not objects:
-        objects = ContestPerson.objects.filter(person__username=username).prefetch_related("contest", "person", "group")
-        cache.set(key, objects, 60)
-    return objects
-
-
-def get_user_contests(request):
-    return [(p.contest.id, p.contest_role) for p in get_user_contest_person_objects(request)]
+def get_username_from_session(request):
+    if not "username" in request.session:
+        username = request.user.username
+        request.session["username"] = username
+    return request.session["username"]
 
 
 def get_current_contest_dict(request):
     if not ("current_contest_id" in request.session and "current_contest_role" in request.session):
-        contests = get_user_contests(request)
+        username = get_username_from_session(request)
+        contests = models_helper.get_person_contests_ids_and_roles(username)
         if len(contests) > 0:
-            request.session["current_contest_id"] = contests[0][0]
+            request.session["current_contest_id"] = contests[0][0].hex
             request.session["current_contest_role"] = contests[0][1]
         else:
             raise Exception("User Have No Contests!")
     return {"id": request.session["current_contest_id"], "role": request.session["current_contest_role"]}
 
 
-def get_contest_person_object(request):
-    contest = get_current_contest_dict(request)
-    contest_person_objects = get_user_contest_person_objects(request)
-    return contest_person_objects.filter(contest__id=contest["id"])[0]
-
-
-def get_contest(request):
-    contest_person_object = get_contest_person_object(request)
-    return contest_person_object.contest
+def get_current_contest_object(request):
+    current_contest_id = get_current_contest_dict(request)["id"]
+    return Contest.objects.get(id=current_contest_id)
