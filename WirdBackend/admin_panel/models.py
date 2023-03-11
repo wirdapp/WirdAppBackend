309ce29421816b967aaf23f1c4306c9972de092a
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:46 -0800

    Update models to limit one person-contest relation in a contest

--- WirdBackend/admin_panel/models.py
+++ WirdBackend/admin_panel/models.py
@@ -1,38 +1,32 @@
-import uuid
-
-from django.contrib.auth.models import Permission
 from django.core.validators import validate_comma_separated_integer_list
 from django.db import models
-from django.utils.functional import cached_property
-
-from core.models import Contest, ContestPerson
 
 
 class Section(models.Model):
     label = models.CharField(default='', max_length=32)
     position = models.IntegerField(default=1)
-    contest = models.ForeignKey(Contest, on_delete=models.PROTECT, related_name='contest_sections')
+    contest = models.ForeignKey("core.Contest", on_delete=models.PROTECT, related_name='contest_sections')
 
     def __str__(self):
         return self.label
 
 
 class PointTemplate(models.Model):
-    FORM_TYPES = (
-        (1, 'Number'),
-        (2, 'Check Box'),
-        (3, 'Other'),
-    )
+    class FormType(models.IntegerChoices):
+        NUM = (1, 'Number')
+        CHECKBOX = (2, 'Check Box')
+        OTHER = (3, 'Other')
+
     is_active = models.BooleanField(default=True)
     is_shown = models.BooleanField(default=True)
     order_in_section = models.IntegerField()
     custom_days = models.CharField(validators=[validate_comma_separated_integer_list], max_length=64, blank=True)
-    contest = models.ForeignKey(Contest, on_delete=models.PROTECT, related_name="contest_point_templates")
+    contest = models.ForeignKey("core.Contest", on_delete=models.PROTECT, related_name="contest_point_templates")
     section = models.ForeignKey(Section, on_delete=models.PROTECT, related_name="contest_sections")
 
     label = models.CharField(max_length=128, default='')
     description = models.CharField(max_length=256, default='')
-    form_type = models.PositiveSmallIntegerField(choices=FORM_TYPES, default=1)
+    form_type = models.PositiveSmallIntegerField(choices=FormType.choices, default=FormType.NUM)
     upper_units_bound = models.IntegerField(default=1)
     lower_units_bound = models.IntegerField(default=0)
     points_per_unit = models.IntegerField(default=1)
@@ -42,18 +36,3 @@ class PointTemplate(models.Model):
 
     def __str__(self):
         return self.label
-
-
-class Group(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100, default='')
-    created_at = models.DateTimeField(auto_now_add=True)
-    announcements = models.TextField(default="", blank=True)
-    contest = models.ForeignKey(Contest, on_delete=models.PROTECT)
-
-    @cached_property
-    def members_count(self):
-        return ContestPerson.objects.filter(contest_role=1, group_role=1, group__id=self.id).count()
-
-    def __str__(self):
-        return self.name
--- WirdBackend/core/models.py
+++ WirdBackend/core/models.py
@@ -1,21 +1,38 @@
 import uuid
 
-from django.contrib.auth.models import User, AbstractUser
-from django.core.validators import integer_validator
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import integer_validator, MinLengthValidator
 from django.db import models
+from django.db.models import Q
+from django.utils.functional import cached_property
 from django_resized import ResizedImageField
 
 
 class Contest(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=128, default='')
+    name = models.CharField(max_length=128, validators=[MinLengthValidator(4)], default='')
+    description = models.TextField(default='')
     show_standings = models.BooleanField(default=True)
     announcements = models.TextField(default="", blank=True)
     readonly_mode = models.BooleanField(default=False, help_text='Stop scoring points and only show scored points')
 
-    @property
+    @cached_property
     def access_code(self):
         return self.id.hex.upper()[-6:]
+
+    @cached_property
+    def admin_count(self):
+        return ContestPerson.objects.filter(contest__id=self.id, contest_role__in=[2, 3]).values_list("person__id") \
+            .distinct().count()
+
+    @cached_property
+    def member_count(self):
+        return ContestPerson.objects.filter(contest__id=self.id, contest_role=1).values_list("person__id") \
+            .distinct().count()
+
+    @cached_property
+    def group_count(self):
+        return Group.objects.filter(contest__id=self.id).count()
 
 
 def upload_location(instance, filename):
@@ -30,26 +47,39 @@ class Person(AbstractUser):
     phone_number = models.CharField(max_length=15, validators=[integer_validator], default="0000000000", blank=True)
 
 
+class Group(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    announcements = models.TextField(default="", blank=True)
+    contest = models.ForeignKey("core.Contest", on_delete=models.PROTECT)
+
+    @cached_property
+    def members_count(self):
+        return ContestPerson.objects.filter(contest_role=1, group__id=self.id).count()
+
+    def __str__(self):
+        return self.name
+
+
 class ContestPerson(models.Model):
-    CONTEST_ROLE = (
-        (1, 'member'),
-        (2, 'admin'),
-        (3, 'super_admin'),
-        (4, 'pending_member'),
-        (5, 'deactivated'),
-    )
-    GROUP_ROLE = (
-        (1, 'member'),
-        (2, 'admin'),
-    )
+    class ContestRole(models.IntegerChoices):
+        MEMBER = (1, 'member')
+        ADMIN = (2, 'admin')
+        SUPER_ADMIN = (3, 'super_admin')
+        PENDING_MEMBER = (4, 'pending_member')
+        DEACTIVATED = (5, 'deactivated')
+
     contest = models.ForeignKey(Contest, on_delete=models.PROTECT, blank=True, null=True)
     person = models.ForeignKey(Person, on_delete=models.PROTECT)
-    group = models.ForeignKey("admin_panel.Group", on_delete=models.PROTECT, blank=True, null=True)
-    contest_role = models.PositiveSmallIntegerField(choices=CONTEST_ROLE, default=1)
-    group_role = models.PositiveSmallIntegerField(choices=GROUP_ROLE, default=1)
+    group = models.ForeignKey(Group, on_delete=models.PROTECT, blank=True, null=True)
+    contest_role = models.PositiveSmallIntegerField(choices=ContestRole.choices, default=ContestRole.MEMBER)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['contest_id', 'person_id', "group", "contest_role","group_role"],
-                                    name="unique_group_person"),
+            models.UniqueConstraint(fields=['contest_id', 'person_id'],
+                                    name="unique_group_person",
+                                    violation_error_message="Can't Create more than one person in a contest"),
+            models.CheckConstraint(check=~Q(contest_role=3) | Q(group=None),
+                                   name="Super Admin can't be assigned to a group")
         ]
