44e03a15848422262eb3a85f692b36114094d918
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:47 -0800

    CheckConstraints can't be applied to joined tables thus removed

--- WirdBackend/member_panel/models.py
+++ WirdBackend/member_panel/models.py
@@ -2,7 +2,6 @@ import datetime
 import uuid
 
 from django.db import models
-from django.db.models import Q
 from polymorphic.models import PolymorphicModel
 
 from admin_panel.models import PointTemplate
@@ -18,10 +17,6 @@ class PointRecord(PolymorphicModel):
 
     class Meta:
         ordering = ('-record_date',)
-        constraints = [
-            models.CheckConstraint(check=Q(person__contest_role=1), name="members_only_check",
-                                   violation_error_message="Only Members can score points")
-        ]
 
     def __str__(self):
         return f'{self.person.person_id}:{self.point_template.label}:date:{self.record_date}'
