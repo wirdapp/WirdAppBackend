64a2bd9b225f91c06062b1db4ac07d3e203c2d42
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:47 -0800

    Add TopMembers APIView

--- WirdBackend/admin_panel/views.py
+++ WirdBackend/admin_panel/views.py
@@ -1,8 +1,17 @@
+from collections import OrderedDict
+
+from django.db.models import Sum, Value
+from django.db.models.functions import Concat
+from rest_condition import And
+from rest_framework import views
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from core.my_view import MyModelViewSet
+from core.permissions import IsContestAdmin
 from core.serializers import PersonSerializer
+from member_panel.models import PointRecord
 from .serializers import *
 
 
@@ -75,3 +84,29 @@ class ContestPeopleView(MyModelViewSet):
     def get_queryset(self):
         current_contest = util.get_current_contest_dict(self.request)["id"]
         return models_helper.get_contest_people(current_contest).values("id", "username", "first_name", "last_name")
+
+
+class TopMembers(views.APIView):
+    permission_classes = [And(IsAuthenticated(), IsContestAdmin()), ]
+
+    def get(self, request, *args, **kwargs):
+        contest_id = util.get_current_contest_dict(request)["id"]
+        top_members = PointRecord.objects.filter(person__contest__id=contest_id) \
+                          .annotate(name=Concat("person__person__first_name", Value(' '), "person__person__last_name")) \
+                          .values("person_id", 'name') \
+                          .annotate(total_points=Sum("point_total")).order_by("-total_points")[:5]
+
+        results = list(PointRecord.objects.filter(person_id__in=top_members.values_list("person_id"))
+                       .values('record_date', 'person_id')
+                       .order_by('-record_date')
+                       .annotate(total_daily_points=Sum("point_total")))
+
+        user_results = OrderedDict()
+        for member in top_members:
+            user_results[member["person_id"]] = {'name': member["name"], 'total': member["total_points"]}
+
+        for result in results:
+            person_id = result['person_id']
+            user_results[person_id].update({str(result['record_date']): result['total_daily_points']})
+
+        return Response(user_results)
