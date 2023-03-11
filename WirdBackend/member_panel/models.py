c71067a89f07863f277ec07f5d09227cf43b19ef
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:47 -0800

    Add CheckConstraint to give only members the ability to score points

--- WirdBackend/member_panel/models.py
+++ WirdBackend/member_panel/models.py
@@ -2,6 +2,7 @@ import datetime
 import uuid
 
 from django.db import models
+from django.db.models import Q
 from polymorphic.models import PolymorphicModel
 
 from admin_panel.models import PointTemplate
@@ -10,13 +11,17 @@ from core.models import ContestPerson
 
 class PointRecord(PolymorphicModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    person = models.ForeignKey(ContestPerson, on_delete=models.PROTECT, null=True, related_name='contest_person_points')
+    person = models.ForeignKey(ContestPerson, on_delete=models.PROTECT, related_name='contest_person_points')
     point_template = models.ForeignKey(PointTemplate, on_delete=models.PROTECT)
     record_date = models.DateField(default=datetime.date.today)
     point_total = models.IntegerField(default=0)
 
     class Meta:
         ordering = ('-record_date',)
+        constraints = [
+            models.CheckConstraint(check=Q(person__contest_role=1), name="members_only_check",
+                                   violation_error_message="Only Members can score points")
+        ]
 
     def __str__(self):
         return f'{self.person.person_id}:{self.point_template.label}:date:{self.record_date}'
