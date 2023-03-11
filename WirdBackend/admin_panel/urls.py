86c6757ce24a44dd08880f428b7791707c29639d
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:49 -0800

    Added the needed routes for the results page

--- WirdBackend/admin_panel/urls.py
+++ WirdBackend/admin_panel/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, include
+import datetime
+
+from django.shortcuts import redirect
+from django.urls import include
+from django.urls import path, register_converter
 from rest_framework import routers
 
+from core.util_classes import DateConverter
 from . import views
 
 router = routers.DefaultRouter()
@@ -9,8 +14,12 @@ router.register(r'point-template', views.PointTemplatesView, basename='PointTemp
 router.register(r'section', views.SectionView, basename='Section')
 router.register(r'groups', views.GroupView, basename='Groups')
 router.register(r'contest-people', views.ContestPersonView, basename='Contest People')
+register_converter(DateConverter, 'date')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('top_members/', views.TopMembers.as_view(), name='top_members'),
+    path('results/', lambda request: redirect('/admin-panel/results/' + datetime.date.today().isoformat())),
+    path('results/<date:date>/', views.ResultsView.as_view(), name='Results Page'),
+    path('results/<date:date>/<str:group_id>', views.GroupMemberResultsView.as_view(), name='Results Page'),
 ]
