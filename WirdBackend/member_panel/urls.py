9d8728afe40fcd96bc617a0b801065df33ca6d0e
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:52 -0800

    Add point-templates to routers

--- WirdBackend/member_panel/urls.py
+++ WirdBackend/member_panel/urls.py
@@ -4,7 +4,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'point_records', views.PointRecordsView, basename='point_records')
+router.register(r'point-records', views.PointRecordsView, basename='point_records')
+router.register(r'point-templates', views.ReadOnlyPointTemplateView, basename='point_templates')
 
 urlpatterns = [
     path('', include(router.urls)),
