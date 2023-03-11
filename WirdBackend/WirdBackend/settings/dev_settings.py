a171a12bdfb35b0e8307208de7f3a94ad1c527cc
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:50 -0800

    Adding LocaleMiddleware to the dev settings

--- WirdBackend/WirdBackend/settings/dev_settings.py
+++ WirdBackend/WirdBackend/settings/dev_settings.py
@@ -32,6 +32,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 LOGGING = {}
 ALLOWED_HOSTS = ["*"]
