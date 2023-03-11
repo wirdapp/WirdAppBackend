616efc558f0cdc83f73fb15ddc184ce42d05c58d
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:45 -0800

    Add Contest People to the Routes

--- WirdBackend/admin_panel/urls.py
+++ WirdBackend/admin_panel/urls.py
@@ -8,6 +8,7 @@ router = routers.DefaultRouter()
 router.register(r'point-template', views.PointTemplatesView, basename='PointTemplate')
 router.register(r'section', views.SectionView, basename='Section')
 router.register(r'groups', views.GroupView, basename='Groups')
+router.register(r'contest_people', views.ContestPeopleView, basename='Contest People')
 
 
 urlpatterns = [
