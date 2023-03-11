ef47eec755e3c8055c60db70c821e37f6770471a
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:46 -0800

    Moved AutoSetContestSerializer from core serializer

--- WirdBackend/admin_panel/serializers.py
+++ WirdBackend/admin_panel/serializers.py
@@ -2,9 +2,16 @@ from rest_framework import serializers
 
 import core.serializers
 from core import models_helper, util
-from core.serializers import AutoSetContestSerializer
+from core.models import Group, ContestPerson
 from core.serializers import ContextFilteredPrimaryKeyRelatedField
 from .models import *
+
+
+class AutoSetContestSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        contest = util.get_current_contest_object(self.context["request"])
+        validated_data["contest"] = contest
+        return super().create(validated_data)
 
 
 class SectionSerializer(AutoSetContestSerializer):
@@ -60,13 +67,13 @@ class AddRemovePersonsToGroup(serializers.Serializer):
         person_ids = validated_data["persons"]
         contest_id = util.get_current_contest_dict(self.context)["id"]
         group_id = validated_data["group_id"]
-        contest_role, group_role = (1, 1)
+        contest_role = 1
         if validated_data["person_type"] == "admin":
-            contest_role, group_role = (2, 2)
+            contest_role = 2
         if validated_data["action"] == "remove":
             ContestPerson.objects.filter(contest__id=contest_id, person__id__in=person_ids, group__id=group_id).delete()
         if validated_data["action"] == "add":
-            defaults = dict(contest_role=contest_role, group_role=group_role)
+            defaults = dict(contest_role=contest_role, group_id=group_id)
             for person_id in person_ids:
-                ContestPerson.objects.update_or_create(contest_id=contest_id, person_id=person_id, group_id=group_id,
+                ContestPerson.objects.update_or_create(contest_id=contest_id, person_id=person_id,
                                                        defaults=defaults)
