ccff6d3aea8221264378b7019586d5843fd15280
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:48 -0800

    Updated doc description

--- WirdBackend/WirdBackend/urls.py
+++ WirdBackend/WirdBackend/urls.py
@@ -22,11 +22,11 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Wird APIs",
         default_version='v1',
-        description="Test description",
+        description="Wird endpoints documentation",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email="help@wird.app"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
