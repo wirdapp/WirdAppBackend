ebcd54c71bfc38fdd4e2d085c47da74abbf83ac5
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:53 -0800

    Fix missed up URLs

--- WirdBackend/admin_panel/urls.py
+++ WirdBackend/admin_panel/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('results/', lambda request: redirect('/admin-panel/results/' + datetime.date.today().isoformat())),
     path('results/<date:date>/', views.ResultsView.as_view(), name='Results Page'),
     path('results/<date:date>/<str:group_id>', views.GroupMemberResultsView.as_view(), name='Results Page'),
+    path('export-results/<str:group_id>', views.ExportResultsView.as_view(), name='Export Results Page'),
 ]
--- WirdBackend/member_panel/urls.py
+++ WirdBackend/member_panel/urls.py
@@ -1,15 +1,17 @@
 import datetime
 
 from django.shortcuts import redirect
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('point-records/', lambda request: redirect('/member-panel/point-records/' + datetime.date.today().isoformat())),
-    path('point-records/<date:date>/', views.ResultsByDateView.as_view(), name='Point Templates Page'),
-    path('point-templates/', lambda request: redirect('/member-panel/point-templates/' + datetime.date.today().isoformat())),
-    path('point-templates/<date:date>/', views.ReadOnlyPointTemplateView.as_view({'get': 'list'}), name='Point Templates Page'),
-
+    path('point-records/',
+         lambda request: redirect('/member-panel/point-records/' + datetime.date.today().isoformat())),
+    path('point-records/<date:date>/', views.ResultsByDateView.as_view({'get': 'list', 'post': 'create'}),
+         name='Point Templates Page'),
+    path('point-templates/',
+         lambda request: redirect('/member-panel/point-templates/' + datetime.date.today().isoformat())),
+    path('point-templates/<date:date>/', views.ReadOnlyPointTemplateView.as_view({'get': 'list'}),
+         name='Point Templates Page'),
 ]
