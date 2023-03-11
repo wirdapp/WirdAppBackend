2fff24760d06ff7777dd6a8e36a53bdc65017b14
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:48 -0800

    Removing polymorphic-rest library and its serializers

--- WirdBackend/member_panel/serializers.py
+++ WirdBackend/member_panel/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_polymorphic.serializers import PolymorphicSerializer
 
 from core.models import ContestPerson
 from core.serializers import ContextFilteredPrimaryKeyRelatedField
@@ -9,27 +8,30 @@ from member_panel.models import PointRecord, UserInputPointRecord
 class PointRecordSerializer(serializers.ModelSerializer):
     point_template = ContextFilteredPrimaryKeyRelatedField(object_name="point_templates")
     person = ContextFilteredPrimaryKeyRelatedField(queryset=ContestPerson.objects)
+    record_type = serializers.ChoiceField(choices=["user_input", ], allow_blank=True, required=False)
 
     class Meta:
         exclude = ["polymorphic_ctype"]
         model = PointRecord
 
+    def to_representation(self, obj):
+        if isinstance(obj, UserInputPointRecord):
+            return UserInputPointRecordSerializer(obj, context=self.context).to_representation(obj)
+        return super(PointRecordSerializer, self).to_representation(obj)
 
-class UserInputPointRecordSerializer(PointRecordSerializer):
+    def to_internal_value(self, data):
+        data = data.copy()
+        record_type = data.pop("record_type")
+        if record_type == "user_input":
+            self.Meta.model = UserInputPointRecord
+            return UserInputPointRecordSerializer(context=self.context).to_internal_value(data)
+        return super(PointRecordSerializer, self).to_internal_value(data)
+
+
+class UserInputPointRecordSerializer(serializers.ModelSerializer):
+    record_type = serializers.ReadOnlyField()
     point_template = ContextFilteredPrimaryKeyRelatedField(object_name="point_templates")
 
     class Meta:
         exclude = PointRecordSerializer.Meta.exclude
         model = UserInputPointRecord
-
-
-class PointTemplatePolymorphicSerializer(PolymorphicSerializer):
-    resource_type_field_name = 'type'
-    resource_names_matching = dict(UserInputPointRecord='user_input', PointRecord='general', )
-    model_serializer_mapping = {
-        UserInputPointRecord: UserInputPointRecordSerializer,
-        PointRecord: PointRecordSerializer,
-    }
-
-    def to_resource_type(self, model_or_instance):
-        return self.resource_names_matching[model_or_instance._meta.object_name]
