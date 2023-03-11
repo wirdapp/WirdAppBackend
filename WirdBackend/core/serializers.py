f632ca6a1c794b221f522691024cc314a52f1190
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:44 -0800

    Add DynamicFieldsCategorySerializer

--- WirdBackend/core/serializers.py
+++ WirdBackend/core/serializers.py
@@ -5,7 +5,23 @@ from core import util
 from core.models import Person, Contest, ContestPerson
 
 
-class PersonSerializer(serializers.ModelSerializer):
+class DynamicFieldsCategorySerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+
+        # Instantiate the superclass normally
+        super().__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class PersonSerializer(DynamicFieldsCategorySerializer):
     class Meta:
         model = Person
         fields = ['id', 'username', 'email', 'phone_number', 'first_name', 'last_name', 'profile_photo']
@@ -23,13 +39,6 @@ class ContestPersonSerializer(serializers.ModelSerializer):
     class Meta:
         fields = "__all__"
         model = ContestPerson
-
-
-class AutoSetContestSerializer(serializers.ModelSerializer):
-    def create(self, validated_data):
-        contest = util.get_contest(self.context["request"])
-        validated_data["contest"] = contest
-        return super().create(validated_data)
 
 
 class CreatorSignupSerializer(serializers.ModelSerializer):
@@ -67,3 +76,50 @@ class ParticipantSignupSerializer(serializers.ModelSerializer):
         else:
             raise serializers.ValidationError({"access_code": "Access Code is not correct"})
         return person
+
+
+class AutoSetContestSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        contest = util.get_current_contest_object(self.context["request"])
+        validated_data["contest"] = contest
+        return super().create(validated_data)
+
+
+class ContextFilteredPrimaryKeyRelatedField(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        request = self.context.get('request', None)
+        contest = util.get_current_contest_dict(request)
+        queryset = super(ContextFilteredPrimaryKeyRelatedField, self).get_queryset()
+        if not request or not queryset:
+            return None
+        queryset = queryset.filter(contest__id=contest["id"])
+        return queryset
+
+
+class MyRelatedField(serializers.RelatedField):
+    display_value_fields = {"id"}
+
+    def to_internal_value(self, data):
+        return self.get_queryset().get(pk=data)
+
+    def to_representation(self, value):
+        return getattr(value, "id")
+
+    def display_value(self, instance):
+        values = []
+        for field in self.display_value_fields:
+            try:
+                values.append(getattr(instance, field))
+            except:
+                values.append("")
+
+        return " ".join(values)
+
+    def get_queryset(self):
+        raise NotImplementedError(
+            '{cls}.to_internal_value() must be implemented for field '
+            '{field_name}.'.format(
+                cls=self.__class__.__name__,
+                field_name=self.field_name,
+            )
+        )
