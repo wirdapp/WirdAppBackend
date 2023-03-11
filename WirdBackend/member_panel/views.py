13cee472ba5f5afb22c2b382d3f128483c30b72f
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:52 -0800

    Update PointRecordsView and ReadOnlyPointTemplateView

--- WirdBackend/member_panel/views.py
+++ WirdBackend/member_panel/views.py
@@ -1,16 +1,33 @@
-from rest_framework.permissions import AllowAny
+from rest_condition import And
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
+from admin_panel.serializers import PointTemplateSerializer
+from core import util_methods, models_helper
+from core.permissions import IsContestMember
 from core.util_classes import MyModelViewSet
 from member_panel.models import PointRecord
 from member_panel.serializers import PointRecordSerializer
 
 
 class PointRecordsView(MyModelViewSet):
-    permission_classes = [AllowAny(), ]
+    super_admin_allowed_methods = []
+    admin_allowed_methods = []
+    member_allowed_methods = ['retrieve', 'list', 'create', 'update', 'partial_update']
     serializer_class = PointRecordSerializer
 
     def get_queryset(self):
-        return PointRecord.objects.all()
+        username = util_methods.get_username_from_session(self.request)
+        return PointRecord.objects.filter(person__person__username=username)
 
-    def get_permissions(self):
-        return AllowAny(),
+
+class ReadOnlyPointTemplateView(mixins.ListModelMixin, GenericViewSet):
+    name = 'points-templates-list'
+    permission_classes = [And(IsAuthenticated(), IsContestMember())]
+    serializer_class = PointTemplateSerializer
+
+    def get_queryset(self):
+        contest_id = util_methods.get_current_contest_dict(self.request)["id"]
+        # TODO: Filter by custom_days
+        return models_helper.get_contest_point_templates(contest_id)
