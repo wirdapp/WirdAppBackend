563e054e3c983e9b5a0f96d1039d79d130e11058
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:51 -0800

    add switch_contest to ContestView

--- WirdBackend/core/views.py
+++ WirdBackend/core/views.py
@@ -1,22 +1,40 @@
+from django.utils.translation import gettext
 from drf_yasg import openapi
-from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets, mixins, permissions
+from drf_yasg.utils import swagger_auto_schema, no_body
+from rest_framework import viewsets, mixins, permissions, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from core.my_view import MyModelViewSet
+from core import util_methods, models_helper
 from core.serializers import *
+from core.util_classes import MyModelViewSet
 
 
 class ContestView(MyModelViewSet):
     serializer_class = ContestSerializer
     name = 'create-contest-view'
-    member_allowed_methods = ['retrieve', 'list']
-    admin_allowed_methods = ['retrieve', 'list']
-    super_admin_allowed_methods = ['retrieve', 'list', 'update', 'partial_update']
+    member_allowed_methods = ['switch_contest', 'retrieve', 'list']
+    admin_allowed_methods = ['switch_contest', 'retrieve', 'list']
+    super_admin_allowed_methods = ['switch_contest', 'retrieve', 'list', 'update', 'partial_update']
 
     def get_queryset(self):
-        username = util.get_username_from_session(self.request)
+        username = util_methods.get_username_from_session(self.request)
         return models_helper.get_person_contests_queryset(username)
+
+    @swagger_auto_schema(request_body=no_body,
+                         responses={200: "contest switched successfully",
+                                    404: "person is not enrolled in this contest"},
+                         manual_parameters=[openapi.Parameter('contest_id', openapi.IN_QUERY,
+                                                              description="uuid of the new contest",
+                                                              type=openapi.TYPE_STRING)])
+    @action(detail=False, methods=['post'])
+    def switch_contest(self, request, *args, **kwargs):
+        contests = self.get_queryset()
+        new_contest = request.data["contest_id"]
+        if contests.filter(contest__id=new_contest).exists():
+            return Response(gettext("contest switched successfully"), status=status.HTTP_200_OK)
+        else:
+            return Response(gettext("person is not enrolled in this contest"), status=status.HTTP_404_NOT_FOUND)
 
 
 class SignUpView(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -42,12 +60,12 @@ class CurrentContestPersonView(MyModelViewSet):
     super_admin_allowed_methods = []
 
     def get_object(self):
-        username = util.get_username_from_session(self.request)
+        username = util_methods.get_username_from_session(self.request)
         return Person.objects.get(username=username)
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         person = self.get_serializer(instance)
-        current_contest = util.get_current_contest_dict(self.request)
+        current_contest = util_methods.get_current_contest_dict(self.request)
         data = dict(person=person.data, contest=current_contest)
         return Response(data)
