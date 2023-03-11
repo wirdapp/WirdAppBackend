62076a4bfd15305cd4356865fc3b6e05a3022851
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:46 -0800

    Add fields to admin panel

--- WirdBackend/admin_panel/admin.py
+++ WirdBackend/admin_panel/admin.py
@@ -4,3 +4,5 @@ from admin_panel.models import *
 
 admin.site.register(Section)
 admin.site.register(PointTemplate)
+admin.site.register(NumberPointTemplate)
+admin.site.register(CheckboxPointTemplate)
