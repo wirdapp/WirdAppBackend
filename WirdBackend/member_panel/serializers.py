badb509a6121dcaed10e73dc839dbd8b3aa53a63
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:47 -0800

    - Added PointRecord Related Serializers

---
+++ WirdBackend/member_panel/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from rest_polymorphic.serializers import PolymorphicSerializer
+
+from core.serializers import ContextFilteredPrimaryKeyRelatedField
+from member_panel.models import PointRecord, UserInputPointRecord
+
+
+class PointRecordSerializer(serializers.ModelSerializer):
+    point_template = ContextFilteredPrimaryKeyRelatedField(object_name="point_templates")
+
+    class Meta:
+        exclude = ["person", "polymorphic_ctype"]
+        model = PointRecord
+
+
+class UserInputPointRecordSerializer(PointRecordSerializer):
+    point_template = ContextFilteredPrimaryKeyRelatedField(object_name="point_templates")
+
+    class Meta:
+        exclude = PointRecordSerializer.Meta.exclude
+        model = UserInputPointRecord
+
+
+class PointTemplatePolymorphicSerializer(PolymorphicSerializer):
+    resource_type_field_name = 'type'
+    resource_names_matching = dict(UserInputPointRecord='user_input', PointRecord='general', )
+    model_serializer_mapping = {
+        UserInputPointRecord: UserInputPointRecordSerializer,
+        PointRecord: PointRecordSerializer,
+    }
+
+    def to_resource_type(self, model_or_instance):
+        return self.resource_names_matching[model_or_instance._meta.object_name]
