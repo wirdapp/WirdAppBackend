f49802f84ddcdc808fe077cad3fde978bb1b52d1
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-10 23:35:50 -0800

    Permission updates to PointRecordsView

--- WirdBackend/member_panel/views.py
+++ WirdBackend/member_panel/views.py
@@ -1,10 +1,16 @@
+from rest_framework.permissions import AllowAny
+
 from core.my_view import MyModelViewSet
 from member_panel.models import PointRecord
 from member_panel.serializers import PointRecordSerializer
 
 
 class PointRecordsView(MyModelViewSet):
+    permission_classes = [AllowAny(), ]
     serializer_class = PointRecordSerializer
 
     def get_queryset(self):
         return PointRecord.objects.all()
+
+    def get_permissions(self):
+        return AllowAny(),
