ea79a44189fcd9eef2e060bc242f308671e605b2
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-03-18 00:33:49 -0700

    URL Changes: - contest -> contests - adding contest/current - adding admin-panel/contest

--- WirdBackend/core/urls.py
+++ WirdBackend/core/urls.py
@@ -4,12 +4,11 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'contest', views.ContestView, basename='contests')
+router.register(r'contests', views.ContestView, basename='contests')
 router.register(r'signup', views.SignUpView, basename='signup')
 
 urlpatterns = [
-    path('current-user/', views.CurrentContestPersonView.as_view({'get': 'retrieve', "post": "update"}),
-         name='current-persons'),
+    path('current-user/', views.CurrentContestPersonView.as_view({'get': 'retrieve', "post": "update"})),
     path("top-members/", views.TopMembersOverall.as_view()),
     path("top-members/<date:date>", views.TopMembersByDate.as_view()),
     path("calendar/", views.CalendarView.as_view()),
