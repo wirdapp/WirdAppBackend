367730cff237ca40c807bc7cb8833fba9994c8ca
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-12-16 09:08:54 -0600

    Added Allauth AccountMiddleware

--- WirdBackend/WirdBackend/settings/dev_settings.py
+++ WirdBackend/WirdBackend/settings/dev_settings.py
@@ -26,7 +26,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
+
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_CREDENTIALS = False
 CORS_ORIGIN_ALLOW_ALL = True
