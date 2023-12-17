1c50ba13cab0526211a6747b0745b2c8990e151a
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-12-17 09:16:17 -0600

    Added dj_restauth to DEFAULT_AUTHENTICATION_CLASSES in the dev settings

--- WirdBackend/WirdBackend/settings/dev_settings.py
+++ WirdBackend/WirdBackend/settings/dev_settings.py
@@ -29,9 +29,12 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
-ALLOWED_HOSTS = ["*"]
-CORS_ALLOW_CREDENTIALS = False
-CORS_ORIGIN_ALLOW_ALL = True
+ALLOWED_HOSTS = [
+    'localhost',
+    '0.0.0.0',
+    "dev.wird.app"
+]
+CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8080',
@@ -42,29 +45,3 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:8000',
 ]
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "osamaabuhamdan@yahoo.com"
-EMAIL_HOST_PASSWORD = "prcOzq603GXEhR9g"
-DEFAULT_FROM_EMAIL = 'Wird App <no-reply@wird.app>'
-
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-    }
-}
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "wird_db",
-        "USER": "osamaabuhamdan",
-        "PASSWORD": "osama1997",
-        "HOST": "localhost",
-        "PORT": "5432",
-        "CONN_MAX_AGE": 60,
-    }
-}
-LOGGING = {}
