b1ff6c0db0a43859b9233d3ab078155873cf1ad7
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-12-26 22:18:11 -0600

    Updated Class names and filters

--- WirdBackend/core/models_helper.py
+++ WirdBackend/core/models_helper.py
@@ -1,11 +1,11 @@
 from collections.abc import Iterable
 
-from admin_panel.models import PointTemplate, Section
-from core.models import ContestPerson, Contest, Person, Group, ContestPersonGroups
+from admin_panel.models import ContestCriterion, Section, Group, ContestPersonGroup
+from core.models import ContestPerson, Contest, Person
 
 
 def get_contest_people(contest_id, contest_role=(1, 2, 3)):
-    return ContestPerson.objects.filter(contest__id=contest_id, contest_role__in=contest_role)
+    return
 
 
 def get_person_contests_ids_and_roles(username, contest_role=(0, 1, 2, 3, 4, 5)):
@@ -21,37 +21,38 @@ def get_person_contests_queryset(username, contest_role=(0, 1, 2, 3)):
     return Contest.objects.filter(id__in=contest_ids)
 
 
-def get_person_managed_groups(username, contest_id):
-    persons = ContestPerson.objects.filter(person__username=username, contest_id=contest_id)
-    is_super_admin = persons.filter(contest_role=3).exists()
-    is_admin = persons.filter(contest_role=2).exists()
-    if is_super_admin:
+def get_person_managed_groups(username, current_contest):
+    contest_id = current_contest["id"]
+    role = current_contest["role"]
+    if role <= ContestPerson.ContestRole.SUPER_ADMIN.value:
         return Group.objects.filter(contest__id=contest_id)
-    elif is_admin:
-        group_ids = ContestPersonGroups.objects.filter(contest_person__person__username=username) \
-            .filter(group_role=2).values_list("group__id", flat=True)
-        return Group.objects.filter(id__in=group_ids)
+    elif role == ContestPerson.ContestRole.ADMIN.value:
+        return (ContestPersonGroup.objects
+                .filter(person__username=username, group__contest__id=contest_id, group_role=1)
+                .values("group"))
+    else:
+        return Group.objects.none()
 
 
 def get_group_admins_person_objects(group_id):
-    person_ids = ContestPersonGroups.objects.filter(group__id=group_id, group_role=2) \
+    person_ids = ContestPersonGroup.objects.filter(group__id=group_id, group_role=2) \
         .values_list('contest_person__person__id', flat=True)
     return Person.objects.filter(id__in=person_ids)
 
 
 def get_group_members_contest_person_ids(group_id):
-    return ContestPersonGroups.objects.filter(group__id=group_id, group_role=1) \
+    return ContestPersonGroup.objects.filter(group__id=group_id, group_role=1) \
         .values_list('contest_person_id', flat=True)
 
 
 def get_group_members_person_objects(group_id):
-    person_ids = ContestPersonGroups.objects.filter(group__id=group_id, group_role=1) \
+    person_ids = ContestPersonGroup.objects.filter(group__id=group_id, group_role=1) \
         .values_list('contest_person__person__id', flat=True)
     return Person.objects.filter(id__in=person_ids)
 
 
 def get_contest_point_templates(contest_id):
-    return PointTemplate.objects.filter(contest__id=contest_id)
+    return ContestCriterion.objects.filter(contest__id=contest_id)
 
 
 def get_contest_sections(contest_id):
