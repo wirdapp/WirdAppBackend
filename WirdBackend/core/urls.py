59e8d248685882a540d3d553209a9f4cc20620fb
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-12-31 18:46:56 -0600

    Removed unused URLs for Now

--- WirdBackend/core/urls.py
+++ WirdBackend/core/urls.py
@@ -8,8 +8,6 @@ router.register(r'contests', views.ContestView, basename='contests')
 
 urlpatterns = [
     path('current_user/', views.CurrentUserView.as_view()),
-    path("top-members/", views.TopMembersOverall.as_view()),
-    path("top-members/<date:date>", views.TopMembersByDate.as_view()),
     path("calendar/", views.CalendarView.as_view()),
     path('', include(router.urls)),
 ]
