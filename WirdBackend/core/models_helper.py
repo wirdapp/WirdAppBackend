7b2a8214cf1b8b0d9e87345452495c4ef4d942c8
oabuhamdan <osamaabuhamdan@yahoo.com>
2023-12-31 18:14:23 -0600

    Updated Middlewares, Installed Apps to remove dependency on Sessions

--- WirdBackend/core/models_helper.py
+++ WirdBackend/core/models_helper.py
@@ -1,6 +1,6 @@
 from admin_panel.models import ContestCriterion, Section, Group, ContestPersonGroup
 from core import util_methods
-from core.models import ContestPerson, Person
+from core.models import ContestPerson, Person, Contest
 
 
 def get_contest_person_objects(username, contest_role=(0, 1, 2, 3, 4)):
@@ -8,13 +8,15 @@ def get_contest_person_objects(username, contest_role=(0, 1, 2, 3, 4)):
 
 
 def get_person_contests(username, contest_role=(0, 1, 2, 3, 4)):
-    return ContestPerson.objects.filter(person__username=username, contest_role__in=contest_role).values("contest")
+    person_contests = get_contest_person_objects(username, contest_role).values_list('contest', flat=True)
+    contests = Contest.objects.filter(id__in=person_contests)
+    return contests
 
 
 def get_current_user_managed_groups(request):
-    contest_id = util_methods.get_current_contest_id_from_session(request)
-    role = util_methods.get_current_user_role_from_session(request)
-    username = util_methods.get_username_from_session(request)
+    contest_id = util_methods.get_current_contest_id(request)
+    role = util_methods.get_current_user_contest_role(request)
+    username = util_methods.get_username(request)
     if role <= ContestPerson.ContestRole.SUPER_ADMIN.value:
         return Group.objects.filter(contest__id=contest_id)
     elif role == ContestPerson.ContestRole.ADMIN.value:
