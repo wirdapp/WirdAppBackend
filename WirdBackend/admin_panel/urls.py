6c63150e60b0b08bce3914977cbddd0ec74e6d54
oabuhamdan <osamaabuhamdan@yahoo.com>
2024-01-14 13:43:56 -0600

    Added nested router for members of a group

--- WirdBackend/admin_panel/urls.py
+++ WirdBackend/admin_panel/urls.py
@@ -1,16 +1,19 @@
 from django.urls import include
 from django.urls import path
-from rest_framework import routers
-
 from . import views
+from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 
 router.register(r'criteria', views.ContestCriterionView, basename='ContestCriterionView')
 router.register(r'section', views.SectionView, basename='Section')
-router.register(r'groups', views.GroupView, basename='Groups')
-router.register(r'person_groups', views.ContestPersonGroupView, basename='PersonGroups')
-router.register(r'contest_people', views.ContestPersonView, basename='Contest People')
+router.register(r'contest_members', views.ContestMembersView, basename='Contest Members')
+
+router.register(r'groups', views.GroupView, basename='groups')
+groups_router = routers.NestedSimpleRouter(router, r'groups', lookup='group')
+groups_router.register(r'members', views.ContestPersonGroupView, basename='group_members')
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(groups_router.urls)),
 ]
