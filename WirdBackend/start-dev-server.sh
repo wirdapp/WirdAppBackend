23de9d58adc0b0ed2151198bdbbf508e66853b35
oabuhamdan <osamaabuhamdan@yahoo.com>
2024-01-15 15:04:07 -0600

    removed migrations from server

--- WirdBackend/start-dev-server.sh
+++ WirdBackend/start-dev-server.sh
@@ -2,8 +2,8 @@
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations
+python manage.py migrate --no-input
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
