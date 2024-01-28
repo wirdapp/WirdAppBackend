c42bb0871ebffc6ff99daaa599ba2c32bd61dfb6
oabuhamdan <osamaabuhamdan@yahoo.com>
2024-01-28 07:26:13 -0600

    Added EmailVerified info to the PersonSerializer

--- WirdBackend/core/serializers.py
+++ WirdBackend/core/serializers.py
@@ -1,12 +1,19 @@
 from core.models import Person, Contest
 from core.util_classes import DynamicFieldsCategorySerializer
+from rest_framework import serializers
+from allauth.account.models import EmailAddress
 
 
 class PersonSerializer(DynamicFieldsCategorySerializer):
+    email_verified = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Person
-        fields = ['username', 'email', 'phone_number', 'first_name', 'last_name', 'profile_photo']
+        fields = ['username', 'email', 'phone_number', 'first_name', 'last_name', 'profile_photo', "email_verified"]
         read_only_fields = ['username']
+
+    def get_email_verified(self, person):
+        return EmailAddress.objects.filter(user=person, verified=True).exists()
 
 
 class ContestSerializer(DynamicFieldsCategorySerializer):
