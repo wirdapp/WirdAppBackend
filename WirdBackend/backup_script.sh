d6bcc3aaa7e1c0e905cd5f32c598bf11178112bd
oabuhamdan <osamaabuhamdan@yahoo.com>
2024-02-16 23:08:36 -0600

    Update condig files

---
+++ WirdBackend/backup_script.sh
@@ -0,0 +1,1 @@
+python manage.py dbbackup --noinput  --quiet --compress
---
+++ WirdBackend/start-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export DJANGO_SETTINGS_MODULE="WirdBackend.settings.settings"
+python manage.py migrate --no-input
+gunicorn -c ./gunicorn_prod.py
