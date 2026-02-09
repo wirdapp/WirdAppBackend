b7e5b5835df3790b044d75f2126153d3ebb69338
oabuhamdan <oabuhamdan@pm.me>
2026-02-09 10:15:19 -0600

    The model Schedule is already registered in app 'django_q'

--- WirdBackend/notifications/admin.py
+++ WirdBackend/notifications/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 
 from notifications.models import UserDevice, Notification, NotificationLog
 
-from django_q.models import Schedule
-
-admin.site.register(Schedule)
 admin.site.register(UserDevice)
 admin.site.register(Notification)
 admin.site.register(NotificationLog)
