e39aca85aca2d8481b0d3a19d8bb4c0410d1c8ed
oabuhamdan <oabuhamdan@pm.me>
2026-02-26 17:55:10 -0600

    Add `ExportJob` model migration and update notification URLs to use separate routers

--- WirdBackend/notifications/urls.py
+++ WirdBackend/notifications/urls.py
@@ -2,9 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from notifications.views import UserDeviceViewSet, AllNotificationViewSet
 
-router = DefaultRouter()
-router.register("devices", UserDeviceViewSet, basename="device")
+devices_router = DefaultRouter()
+devices_router.register("devices", UserDeviceViewSet, basename="device")
+
+notifications_router = DefaultRouter()
+notifications_router.register(r"(?P<contest_id>[^/.]+)", AllNotificationViewSet, basename="all-notifications")
+
 urlpatterns = [
-    path('', include(router.urls)),
-    path("<str:contest_id>/all/",AllNotificationViewSet.as_view(),name="all-notifications",),
+    path('', include(devices_router.urls)),
+    path('', include(notifications_router.urls)),
 ]
